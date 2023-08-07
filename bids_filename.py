5d40d2d96d7c741c0b80d4f2ae2c89cd7eace8e7
erin seepersad <105820206+erinseepersad@users.noreply.github.com>
2023-08-07 12:27:56 -0400

    Add files via upload

--- bids_filename.py
+++ bids_filename.py
@@ -6,7 +6,7 @@ Created on Tue Jul 18 13:18:06 2023
 """
 
 #class-->object--> variables and functions
-
+"""
 # example from website
 class Person:
   def __init__(self, name, age):
@@ -17,7 +17,7 @@ p1 = Person("John", 36)
 
 print(p1.name)
 print(p1.age)
-"""
+
 filename template
 key1-value1_key2-value2_suffix.extension
 
@@ -30,19 +30,50 @@ print(bids_path)
 #sub-aliens9_ses-payoff1_task-rocket1_acq-quicktutorial1_mod-raw_run-s20031.iqdat
 from mne_bids import BIDSPath #website 
 import os
+import datetime
+import pandas as pd 
 
 bids_path = BIDSPath(subject='aliens9', session='payoff1', task='rocket1', acquisition='quicktutorial1', modality='raw', run='s20031', suffix='raw', extension='.iqdat')
 print(bids_path)
 
-#insert the file name 
-input_file = input("Enter the input file path: ")
+#grabbing the file from the directory
+
+def get_file_from_directory(directory_path, file_name):
+    if not os.path.isdir(directory_path):
+        print("Error: The provided path is not a directory.")
+        return None
+
+    #list of files in the directory 
+    file_list = os.listdir(directory_path)
+
+    # Search for the desired file in the list
+    for file in file_list:
+        if file == file_name:
+            return os.path.join(directory_path, file)
+
+    # If the file is not found, return None
+    return None
+
+# Replace these values with your actual directory path and file name
+directory_path = "/path/to/your/directory"
+file_name = "your_file.txt"
+
+#full file path
+file_path = get_file_from_directory(directory_path, file_name)
+
+# Check if the file was found and print the result
+if file_path:
+    print("File found at:", file_path)
+else:
+    print("File not found in the directory.")
+
 #convert to BIDS
 def conversion_filename (given_filename):
     breakingdown= input_file_name.split('_')
     #convert to BIDS format
     """
-    subject_label = 
-    session_label = 
+    subject_label = narc iD
+    session_label = the date 
     task_label =
     acquisition_label = 
     modality_label = 

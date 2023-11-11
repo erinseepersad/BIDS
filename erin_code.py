c67dc51c4cefe0540ff9590e97f90475d74e13df
erin seepersad <105820206+erinseepersad@users.noreply.github.com>
2023-11-11 01:45:32 -0500

    Add files via upload

---
+++ erin_code.py
@@ -0,0 +1,12 @@
+#os.listdir('directory_path') function
+# cd?
+# instructions: look at OS and create something where you can get a list of all the directories stored
+import os
+# my directory path
+directory_path= '/Users/qercA/Downloads/mount_data'
+# getting a list of all the files in the directory from the previous line
+allentries = os.listdir(directory_path)
+# make a list of the files 
+print ('List of files')
+for entry in allentries:
+    print(entry)
